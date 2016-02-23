ce5e6656391c22c59d7cc7ab81352c26e314b9b3
Kaan0200 <aka43@case.edu>
2016-02-22 20:14:21 -0500

    Added some intial local DB script files and framework for adding more. The proper usage of which will be discussed later

---
+++ ios/DB Scripts/Local_Tables.sql
@@ -0,0 +1,32 @@
+----------
+--This script file contains all the creation of tables and their various dependencies for the LOCAL SQL
+----------
+
+-- all pages for heirachical structure
+CREATE TABLE pages (
+  id               integer PRIMARY KEY,
+  parent_id        integer,
+  title            text,
+  subtitle         text,
+  is_bookmarked    integer(1),
+  allow_bookmarked integer(1),
+  text             blob,
+  link_text        text,
+  content_id       integer
+);
+
+-- table for looking up content ids matched with the location on disk
+CREATE TABLE contents (
+  id       integer PRIMARY KEY,
+  location text
+);
+
+-- table for verifying that the local DB is up to date with the Master one
+CREATE TABLE db_version (
+  id           integer PRIMARY KEY,
+  version      text,
+  updated_date text
+);
+
+
+
