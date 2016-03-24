3f686cfa98318ff52b960a76ea2817d8e388d302
Andrew <agm67@case.edu>
2016-03-24 14:13:09 -0400

    chnaged the if statement blocks to switch statments in the get score method of PalliativePerformanceCalculatorViewController

--- ios/Palliative Mobile/PalliativePerformanceCalculatorViewController.swift
+++ ios/Palliative Mobile/PalliativePerformanceCalculatorViewController.swift
@@ -176,41 +176,87 @@ class PalliativePerformanceCalculatorViewController: UIViewController, UIPopover
     
     func getScore() -> String {
         
-        // set score from button1 (ambulation)
-        if button1 == ambulationOptions[0] {for i in 0...2 {scoreTracker[i]++}}
-        if button1 == ambulationOptions[1] {for i in 3...4 {scoreTracker[i]++}}
-        if button1 == ambulationOptions[2] {scoreTracker[5]++}
-        if button1 == ambulationOptions[3] {scoreTracker[6]++}
-        if button1 == ambulationOptions[4] {for i in 7...9 {scoreTracker[i]++}}
+        // set score from button 1 (ambulation)
+        switch button1 {
+        case ambulationOptions[0] : for i in 0...2 {scoreTracker[i]++}
+        case ambulationOptions[1] : for i in 3...4 {scoreTracker[i]++}
+        case ambulationOptions[2] : scoreTracker[5]++
+        case ambulationOptions[3] : scoreTracker[6]++
+        default : for i in 7...9 {scoreTracker[i]++}
+        }
         
-        // set score from button2 (activity & evidence of disease)
-        if button2 == activityOptions[0] {scoreTracker[0]++}
-        if button2 == activityOptions[1] {scoreTracker[1]++}
-        if button2 == activityOptions[2] {scoreTracker[2]++}
-        if button2 == activityOptions[3] {scoreTracker[3]++}
-        if button2 == activityOptions[4] {scoreTracker[4]++}
-        if button2 == activityOptions[5] {scoreTracker[5]++}
-        if button2 == activityOptions[6] {scoreTracker[6]++}
-        if button2 == activityOptions[7] {for i in 7...9 {scoreTracker[i]++}}
+        // set score from button 2 (activity & eveidence)
+        switch button2 {
+        case activityOptions[0] : scoreTracker[0]++
+        case activityOptions[1] : scoreTracker[1]++
+        case activityOptions[2] : scoreTracker[2]++
+        case activityOptions[3] : scoreTracker[3]++
+        case activityOptions[4] : scoreTracker[4]++
+        case activityOptions[5] : scoreTracker[5]++
+        case activityOptions[6] : scoreTracker[6]++
+        default : for i in 7...9 {scoreTracker[i]++}
+        }
         
         // set score from button3 (self care)
-        if button3 == selfCareOptions[0] {for i in 0...3 {scoreTracker[i]++}}
-        if button3 == selfCareOptions[1] {scoreTracker[4]++}
-        if button3 == selfCareOptions[2] {scoreTracker[5]++}
-        if button3 == selfCareOptions[3] {scoreTracker[6]++}
-        if button3 == selfCareOptions[4] {for i in 7...9 {scoreTracker[i]++}}
+        switch button3 {
+        case selfCareOptions[0] : for i in 0...3 {scoreTracker[i]++}
+        case selfCareOptions[1] : scoreTracker[4]++
+        case selfCareOptions[2] : scoreTracker[5]++
+        case selfCareOptions[3] : scoreTracker[6]++
+        default : for i in 7...9 {scoreTracker[i]++}
+        }
         
         // set score from button4 (intake)
-        if button4 == intakeOptions[0] {for i in 0...1 {scoreTracker[i]++}}
-        if button4 == intakeOptions[1] {for i in 2...7 {scoreTracker[i]++}}
-        if button4 == intakeOptions[2] {scoreTracker[8]++}
-        if button4 == intakeOptions[3] {scoreTracker[9]++}
+        switch button4 {
+        case intakeOptions[0] : for i in 0...1 {scoreTracker[i]++}
+        case intakeOptions[1] : for i in 2...7 {scoreTracker[i]++}
+        case intakeOptions[2] : scoreTracker[8]++
+        default : scoreTracker[9]++
+        }
         
         // set score from button5 (conscious level)
-        if button5 == consciousOptions[0] {for i in 0...3 {scoreTracker[i]++}}
-        if button5 == consciousOptions[1] {for i in 4...5 {scoreTracker[i]++}}
-        if button5 == consciousOptions[2] {for i in 6...8 {scoreTracker[i]++}}
-        if button5 == consciousOptions[3] {scoreTracker[9]++}
+        switch button5 {
+        case consciousOptions[0] : for i in 0...3 {scoreTracker[i]++}
+        case consciousOptions[1] : for i in 4...5 {scoreTracker[i]++}
+        case consciousOptions[2] : for i in 6...8 {scoreTracker[i]++}
+        default : scoreTracker[9]++
+        }
+        
+//        // set score from button1 (ambulation)
+//        if button1 == ambulationOptions[0] {for i in 0...2 {scoreTracker[i]++}}
+//        if button1 == ambulationOptions[1] {for i in 3...4 {scoreTracker[i]++}}
+//        if button1 == ambulationOptions[2] {scoreTracker[5]++}
+//        if button1 == ambulationOptions[3] {scoreTracker[6]++}
+//        if button1 == ambulationOptions[4] {for i in 7...9 {scoreTracker[i]++}}
+//        
+//        // set score from button2 (activity & evidence of disease)
+//        if button2 == activityOptions[0] {scoreTracker[0]++}
+//        if button2 == activityOptions[1] {scoreTracker[1]++}
+//        if button2 == activityOptions[2] {scoreTracker[2]++}
+//        if button2 == activityOptions[3] {scoreTracker[3]++}
+//        if button2 == activityOptions[4] {scoreTracker[4]++}
+//        if button2 == activityOptions[5] {scoreTracker[5]++}
+//        if button2 == activityOptions[6] {scoreTracker[6]++}
+//        if button2 == activityOptions[7] {for i in 7...9 {scoreTracker[i]++}}
+//        
+//        // set score from button3 (self care)
+//        if button3 == selfCareOptions[0] {for i in 0...3 {scoreTracker[i]++}}
+//        if button3 == selfCareOptions[1] {scoreTracker[4]++}
+//        if button3 == selfCareOptions[2] {scoreTracker[5]++}
+//        if button3 == selfCareOptions[3] {scoreTracker[6]++}
+//        if button3 == selfCareOptions[4] {for i in 7...9 {scoreTracker[i]++}}
+//        
+//        // set score from button4 (intake)
+//        if button4 == intakeOptions[0] {for i in 0...1 {scoreTracker[i]++}}
+//        if button4 == intakeOptions[1] {for i in 2...7 {scoreTracker[i]++}}
+//        if button4 == intakeOptions[2] {scoreTracker[8]++}
+//        if button4 == intakeOptions[3] {scoreTracker[9]++}
+//        
+//        // set score from button5 (conscious level)
+//        if button5 == consciousOptions[0] {for i in 0...3 {scoreTracker[i]++}}
+//        if button5 == consciousOptions[1] {for i in 4...5 {scoreTracker[i]++}}
+//        if button5 == consciousOptions[2] {for i in 6...8 {scoreTracker[i]++}}
+//        if button5 == consciousOptions[3] {scoreTracker[9]++}
         
         
         return PPSlevels[getFinalScoreIndex()]
@@ -230,15 +276,5 @@ class PalliativePerformanceCalculatorViewController: UIViewController, UIPopover
         
         return sum / divisor
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
