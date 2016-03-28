7539ea8f123bf5632a2991f280921cf6cb34c54f
Andrew <agm67@case.edu>
2016-03-28 18:26:07 -0400

    added the buttons and data to the karnofsky view controller

--- ios/Palliative Mobile/Palliative Mobile/KarnofskyViewController.swift
+++ ios/Palliative Mobile/Palliative Mobile/KarnofskyViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 
 class KarnofskyViewController: UIViewController {
 
+    @IBOutlet weak var condition: UIButton!
+    @IBOutlet weak var levelOfFunctionalCapacity: UIButton!
+    
+    var conditions = [
+        "Able to carry on normal activity and work; no special care needed",
+        "Unable to work; able to live at home and care for most personal needs; varying amount of assitance needed",
+        "Unable to care for self; requires equivalent of instatutional or hospital care; diseases may be progressing rapidly"
+    ]
+    
+    var levelsOfFunctionalCpacity = [
+        "No complaints, no evidence of disease",
+        "Able to carry on normal activity; minor signs or symptoms of disease",
+        "Normal activity with effort; some signs or symptoms of disease",
+        "Care for self; unable to carry on normal activity or to do active work",
+        "Requires occasional assistance but is able to care for most personal needs",
+        "Requires considerable assistance and frequent medical care",
+        "Disabled; requires special care and assistance",
+        "Severely disabled; hospital admission indicated although death not imminent",
+        "very sick; hospital admission necessary; active supportive treatment necessary",
+        "Moribund; fatal processes progressing rapidly",
+        "Death"
+    ]
+    
+    var button1Type = "Condition"
+    var button2Type = "Level of Functional Capacity"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        condition.setTitle(button1Type, forState: UIControlState.Normal)
+        levelOfFunctionalCapacity.setTitle(button2Type, forState: UIControlState.Normal)
 
         // Do any additional setup after loading the view.
     }
